66bf0ca5d26e0e0c1c7541d5e9d941add61dc800
qiudb2021 <qiudb2022@outlook.com>
2021-10-27 15:51:39 +0800

    测试skynet.uniqueservice接口

---
+++ service/test-unique.lua
@@ -0,0 +1,16 @@
+local skynet = require "skynet"
+local args = {...}
+if #args == 0 then
+    table.insert( args,"test-uniqueservice" )
+end
+
+skynet.start(function (  )
+    local us
+    skynet.error("test unique service")
+    if #args == 2 and args[1] == "true" then
+        us = skynet.uniqueservice(true, args[2])
+    else
+        us = skynet.uniqueservice(args[1])
+    end
+    skynet.error("uniqueservice handler: ", skynet.address(us))
+end)
---
+++ service/test-uniqueservice.lua
@@ -0,0 +1,4 @@
+local skynet = require "skynet"
+skynet.start(function (  )
+    skynet.error("Service First Test")
+end)
