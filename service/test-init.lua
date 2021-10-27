babcd436c2ff0fa32cf136251707e82a41763776
qiudb2021 <qiudb2022@outlook.com>
2021-10-27 14:23:44 +0800

    skynet.init的使用

---
+++ service/test-init.lua
@@ -0,0 +1,12 @@
+--[[
+    skynet.init用来注册服务初始化之前，需要执行的函数。也就是在skynet.start之前运行。
+]]
+local skynet = require "skynet"
+
+skynet.init = function (  )
+    skynet.error("service init.")
+end
+
+skynet.start = function (  )
+    skynet.error("service start")
+end
