6b678e69d02615ba05535ff47f4b91e0d19d7103
qiudb2021 <qiudb2022@outlook.com>
2021-10-27 15:56:02 +0800

    测试skynet.uniqueservice接口

--- service/test-uniqueservice.lua
+++ service/test-uniqueservice.lua
@@ -1,6 +1,6 @@
 local skynet = require "skynet"
 skynet.start(function (  )
-    skynet.error("Service First Test")
+    skynet.error("Unique Service Start")
     -- 不要尝试服务初始化阶段退出服务，唯一服会创建失败
     skynet.exit()
 end)
