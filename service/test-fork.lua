9968b892bfa062d1c3208ba08a4d2c4d893f4175
qiudb2021 <qiudb2022@outlook.com>
2021-10-27 20:22:56 +0800

    在服务中开启新的线程 skynet.fork

---
+++ service/test-fork.lua
@@ -0,0 +1,13 @@
+local skynet = require "skynet"
+
+function task( timeout )
+    skynet.error("start co: ", coroutine.running( ))
+    skynet.error("begin sleep")
+    skynet.sleep(timeout)
+    skynet.error("begin end")
+end
+
+skynet.start(function (  )
+    skynet.error("start co: ", coroutine.running( ))
+    skynet.fork(task, 500)
+end)
