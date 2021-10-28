4fd68e42744f5f65e0412584649cb3e74cc4302e
qiudb2021 <qiudb2022@outlook.com>
2021-10-28 17:26:39 +0800

    send call ret

---
+++ service/test-mydb-server.lua
@@ -0,0 +1,27 @@
+local skynet = require "skynet"
+require "skynet.manager"
+local db = {}
+
+local command = {};
+
+function command.GET( key )
+    return db[key]
+end
+
+function command.SET( key, value )
+    db[key] = value
+end
+
+skynet.start(function (  )
+    skynet.dispatch("lua", function ( session, address, cmd, ... )
+        cmd = cmd:upper()
+        local f = command[cmd]
+        if f then
+            skynet.retpack(f(...))
+        else
+            skynet.error(string.format( "Unknown command %s", tostring(cmd) ))
+        end
+    end)
+
+    skynet.register(".mydb")
+end)
