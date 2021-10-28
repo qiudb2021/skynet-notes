829ee36e54e21d323099584a444890f13df71cf3
qiudb2021 <qiudb2022@outlook.com>
2021-10-28 11:05:53 +0800

    长时间占用执行权限的任务

---
+++ service/tesk-busytask.lua
@@ -0,0 +1,15 @@
+local skynet = require "skynet"
+
+function task( name )
+    local i = 0
+    skynet.error(name, "begin task")
+    while i < 2000*10000 do
+        i = i + 1
+    end
+    skynet.error(name, "end task", i)
+end
+
+skynet.start(function (  )
+    skynet.fork(task, "task1")
+    skynet.fork(task, "task2")
+end)
