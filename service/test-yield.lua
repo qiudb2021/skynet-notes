e3479b33da6912a1f220750ada538445b8af2bab
qiudb2021 <qiudb2022@outlook.com>
2021-10-28 11:19:29 +0800

    使用skynet.yield让出执行权

---
+++ service/test-yield.lua
@@ -0,0 +1,19 @@
+local skynet = require "skynet"
+
+function task( name )
+    local i = 0
+    skynet.error(name, "begin task")
+    while i < 20000*10000 do
+        i = i + 1
+        if i % 500 * 10000 == 0 then
+            skynet.yield()
+            skynet.error(name, "task yield")
+        end
+    end
+    skynet.error(name, "end task", i)
+end
+
+skynet.start(function (  )
+    skynet.fork(task, "task1")
+    skynet.fork(task, "task2")
+end)
