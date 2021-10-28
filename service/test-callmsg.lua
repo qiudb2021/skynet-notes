a10d42bf98f8cf6d97d6b28e4342686f6d721e5b
qiudb2021 <qiudb2022@outlook.com>
2021-10-28 16:34:07 +0800

    test skynet.retpack

--- service/test-callmsg.lua
+++ service/test-callmsg.lua
@@ -4,10 +4,12 @@ require "skynet.manager"
 skynet.start(function (  )
     skynet.register(".testcallmsg")
     -- 发送lua类型的消息，发送成功，该函数将阻塞等待响应返回，r的值为响应返回值
-    local r = skynet.call(".testluamsg", "lua", 1000, "skynet call", true)
+    local r = table.pack(skynet.call(".testluamsg", "lua", 1000, "skynet call", true))
     skynet.error("skynet.call return value ", r)
 
-    r = skynet.unpack(skynet.rawcall(".testluamsg", "lua", skynet.pack(1001, "skynet rawcall", false)))
-    skynet.error("skynet.rawcall return value ", r)
-
+    r = table.pack(skynet.unpack(skynet.rawcall(".testluamsg", "lua", skynet.pack(1001, "skynet rawcall", false))))
+    for k, v in pairs(r) do
+        skynet.error("skynet.rawcall return value ", v)
+        
+    end
 end)
