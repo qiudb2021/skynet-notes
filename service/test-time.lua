3edc62e50d54dadfa8a4f3984f3a3578f3385e69
qiudb2021 <qiudb2022@outlook.com>
2021-10-28 13:53:37 +0800

    获取时间

---
+++ service/test-time.lua
@@ -0,0 +1,12 @@
+local skynet = requre "skynet"
+function task(  )
+    skynet.error("task")
+    skynet.error("start time", skynet.starttime())
+    skynet.sleep(200)
+    skynet.error("time", skynet.time())
+    skynet.error("now", skynet.now())
+end
+
+skynet.start(function (  )
+    skynet.fork(task)
+end)
