8b55d4a23727bc1426b02837214507a5de420f84
qiudb2021 <qiudb2022@outlook.com>
2021-11-04 11:01:57 +0800

    modify

--- service/test-snax-service.lua
+++ service/test-snax-service.lua
@@ -1,12 +1,12 @@
 local skynet = require "skynet"
 local snax = require "skynet.snax"
 
--- snax服务初始化时会调用该回调函数，可以获取到启动参数
+
+
 function init( ... )
     skynet.error("snax server start: ", ...)
 end
 
--- snax服务退出时会调用该回调函数，可以获取到退出参数
 function exit( ... )
     skynet.error("snax server exit: ", ...)
 end
