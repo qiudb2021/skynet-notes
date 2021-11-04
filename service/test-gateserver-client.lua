6f1f46db92abb6d49e8e971eaff871f696c7e1f4
qiudb2021 <qiudb2022@outlook.com>
2021-11-04 20:28:38 +0800

    test

--- service/test-gateserver-client.lua
+++ service/test-gateserver-client.lua
@@ -1,9 +1,9 @@
 local skynet = require "skynet"
 local socket = require "client.socket"
-local netpack = require "skynet.netpack"
+-- local netpack = require "skynet.netpack"
 
 skynet.start(function (  )
-    local fd = socket.connect("127.0.0.1", 8001)
-    socket.send(fd, string.pack(">s2", "skynet"))
-    skynet.exit()
+    -- local fd = socket.connect("127.0.0.1", 8001)
+    -- socket.send(fd, string.pack(">s2", "skynet"))
+    -- skynet.exit()
 end)
