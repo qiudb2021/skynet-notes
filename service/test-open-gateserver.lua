6fa64f369c4a42570255f42905770d7c66255287
qiudb2021 <qiudb2022@outlook.com>
2021-11-04 20:01:24 +0800

    max client limit

--- service/test-open-gateserver.lua
+++ service/test-open-gateserver.lua
@@ -7,7 +7,7 @@ skynet.start(function (  )
     -- 需要给网关服务发送open消息，来启动监听
     skynet.call(gateserver, "lua", "open", {
         port = 8001, -- 监听端口
-        maxclient = 50, -- 客户端最大连接数
+        maxclient = 2, -- 客户端最大连接数
         nodelay = true, -- 是否延迟TCP
     })
 
