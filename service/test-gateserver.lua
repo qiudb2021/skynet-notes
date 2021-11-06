7a42f8fd0fe72f62537a0150f65d8eae970d28ab
qiudb2021 <qiudb2022@outlook.com>
2021-11-06 14:43:18 +0800

    test gateserver

--- service/test-gateserver.lua
+++ service/test-gateserver.lua
@@ -17,4 +17,19 @@ function handler.message( fd, msg, sz )
     skynet.error("recv message from fd: ", fd, netpack.tostring(msg, sz))
 end
 
+function handler.error(fd, msg)
+    gateserver.closeclient(fd)
+end
+
+function handler.warning(fd, size)
+    skynet.error("warning fd = ", fd, "unsent data over 1MB")
+end
+
+function handler.open( source, conf )
+    skynet.error("open by ", skynet.address(source))
+    skynet.error("listen on ", conf.port)
+    skynet.error("client max ", conf.maxclient)
+    skynet.error("nodelay ", conf.nodelay)
+end
+
 gateserver.start(handler)
