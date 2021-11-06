8257da544ee43d0f1e60cd8ae3a42019b4b7acab
qiudb2021 <qiudb2022@outlook.com>
2021-11-06 14:01:28 +0800

    send msg to gateserver

--- service/test-gateserver-client.lua
+++ service/test-gateserver-client.lua
@@ -1,4 +1,9 @@
 local skynet = require "skynet"
+local socket = require "client.socket"
 skynet.start(function (  )
     skynet.error("test-gateserver-client")
+    local fd = socket.connect("127.0.0.1", 8001)
+    assert(fd)
+    local bytes = string.pack(">Hc13", 13, "login,101,134")
+    socket.send(fd, bytes)
 end)
