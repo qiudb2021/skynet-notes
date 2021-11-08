113409e745e1f543df6e1bf916ec759295a12e6b
qiudb2021 <qiudb2022@outlook.com>
2021-11-08 14:00:11 +0800

    test dhexchange

--- service/test-dhexchange.lua
+++ service/test-dhexchange.lua
@@ -11,4 +11,13 @@ print("ckey:\t", crypt.hexencode(ckey))
 local serverkey = "22222222"
 print("serverkey: ", serverkey)
 local skey = crypt.dhexchange(serverkey)
-print("skey:\t", crypt.hexencode(skey));
+print("skey:\t", crypt.hexencode(skey))
+
+local cserect = crypt.dhsecret(skey, clientkey);
+print("use skey clientkey dhsecret: ", crypt.hexencode(cserect));
+
+local sserect = crypt.dhsecret(ckey, serverkey)
+print("use ckey serverkey dhsecret: ", crypt.hexencode(sserect))
+
+local sserect = crypt.dhsecret(ckey, skey)
+print("user ckey skey dhsecret:\t", crypt.hexencode(sserect))
